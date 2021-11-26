009320d6c694b4207a564cc73fedd4c75395c00d
Jaswant Chaudhary <jaswantchaudhary@Jaswants-MacBook-Air.local>
2021-11-26 13:24:02 +0530

    Start,Pause, Reset Functionality have been added

--- react-stop-watch/src/App.js
+++ react-stop-watch/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
 
-function App() {
+export default function App() {
+  const [state, setState] = useState(0);
+
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    return pauseTimer;
+  }, []);
+
+  const startTimer = () => {
+    if (timerRef.current === null)
+      timerRef.current = setInterval(() => {
+        setState((prev) => prev + 1);
+      }, 1000);
+  };
+
+  const pauseTimer = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+  };
+
+  const resetTimer = () => {
+    pauseTimer();
+    setState(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="timer-head"> Stop Watch Timer </h1>
+      <h3 className="timer">{state}</h3>
+      <button onClick={startTimer} className="watch-btn">START</button>
+      <button onClick={pauseTimer} className="watch-btn">PAUSE</button>
+      <button onClick={resetTimer} className="watch-btn">RESET</button>
     </div>
   );
 }
-
-export default App;
